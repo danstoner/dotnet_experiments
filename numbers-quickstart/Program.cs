7489c5670dc4fe472335a9c1b6f3965f2f7b8729
Dan Stoner <danstoner@gmail.com>
2019-10-20 11:46:05 -0400

    add numbers tutorial

---
+++ numbers-quickstart/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static int AddTwo(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int MultiplyTwo(int a, int b)
+        {
+            return a * b;
+        }
+
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Numbers");
+            // WorkingWithIntegers();
+            int a = 5;
+            int b = 3;
+            Console.WriteLine("AddTwo: " + AddTwo(a,b) );
+            Console.WriteLine("MultiplyTwo: " + MultiplyTwo(a,b));
+        }
+
+    }
+}
