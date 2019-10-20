394f88ee1de71507dd7175954b119fb2dbf6e1cd
Dan Stoner <danstoner@gmail.com>
2019-10-20 11:55:39 -0400

    add a FizzBuzz

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 16; i++)
+            {
+                if (i % 3 == 0) {
+                    Console.Write("Fizz");
+                }
+                if (i % 5 == 0) {
+                    Console.Write("Buzz");
+                }
+                if (! ((i % 3 == 0) | (i % 5 == 0))) {
+                    Console.Write(i);
+                } 
+                Console.WriteLine("");
+            }
+        }
+    }
+}
